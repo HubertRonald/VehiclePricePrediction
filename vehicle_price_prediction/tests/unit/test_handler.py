ad8354f1da55df4b24728bf4467ce37648e7943e
Ronald Mendoza <ronaldmendoza@Ronalds-iMac.local>
2024-04-26 14:26:00 -0500

    fix architecture

--- vehicle_price_prediction/tests/unit/test_handler.py
+++ vehicle_price_prediction/tests/unit/test_handler.py
@@ -26,4 +26,4 @@ def test_lambda_handler(apigw_event):
     data = json.loads(ret['body'])
 
     assert ret["statusCode"] == 200
-    assert data["body"]["prediction"] == 20876.814453
+    assert data["body"]["prediction"] == 20876.814453125
